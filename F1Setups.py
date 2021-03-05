d2f7d84c47e91942685d3e7b4d9beb48c13fd1f8
scruffe <78relyod@gmail.com>
2021-03-05 05:31:21 +0100

    sa

--- F1Setups.py
+++ F1Setups.py
@@ -19,7 +19,7 @@ root.title('F1 Setup editor')
 root.iconbitmap(str(F1SetupPath)+'/pog.ico')
 
 s = ttk.Style() #s.theme_names('winnative', 'clam', 'alt', 'default', 'classic', 'vista', 'xpnative')
-root.tk.call('lappend', 'auto_path', str(F1SetupPath)) #'C:/Users/88/hello/awthemes-10.2.1/awthemes-10.2.1'
+root.tk.call('lappend', 'auto_path', str(F1SetupPath)) #'C:/Users//hello/awthemes-10.2.1/awthemes-10.2.1'
 root.tk.call('package', 'require', 'awdark')
 s.theme_use('awdark')
 
@@ -27,18 +27,16 @@ steamappid = "1080110" #f12020 steam id
 WorkshopID = "2403338074" #f2 2404403390
 #WorkshopFile = "D:/SteamLibrary/steamapps/workshop/content/1080110/"+ WorkshopID +"/ugcitemcontent.bin"
 WorkshopUrl = "https://steamcommunity.com/sharedfiles/filedetails/?id="+ WorkshopID
-SetupDir = str(F1SetupPath) + "/Setups/" #"c:\Users\88\hello\F1Setups/Setups/
+SetupDir = str(F1SetupPath) + "/Setups/" #"c:\Users\\hello\F1Setups/Setups/
 setupStructFormat = '<4s5l1b 11sfb 13sb 20sb 11s3b 12s2b 16sfb 16s2b 13s9b 19s2b 14sfb13sfb15sfb16sfb16sfb15sfb13sfb12sfb20sfb19sfb27sfb26sfb22sfb21sfb18sfb14sfb29sfb28sfb28sfb27sfb11sfb13sfb21sfb21s8B'
-
-
-with open('tracks.json', encoding='utf-8') as json_file: 
-    tracks = json.load(json_file) 
-
 
 bg = "#33393b"
 fg = "white"
 scale_length = "200" #length of all the sliders
 scale_relief = "flat"
+
+with open('tracks.json', encoding='utf-8') as json_file: 
+    tracks = json.load(json_file) 
 
 countrynames = list(tracks)
 
@@ -116,6 +114,8 @@ weather = StringVar()
 statusmsg = StringVar()
 autoUse = BooleanVar()
 
+#set row integer used for deciding what role a slider(scale) goes
+ri = 0 
 
 # Create and grid the outer content frame
 c = ttk.Frame(root, padding=(5, 5, 12, 0))
@@ -129,16 +129,14 @@ except:
     hkey = None
     messagebox.showerror("error","Can't find steam registry key")
 
-
 try:
     steam_path = winreg.QueryValueEx(hkey, "InstallPath")
     steam_path = steam_path[0]
 except:
     messagebox.showerror("error","Can't find steam Install directory")
     steam_path = filedialog.askdirectory()
-    
-winreg.CloseKey(hkey)
 
+winreg.CloseKey(hkey)
 
 try:    #get WorkshopFile install dir
     libraryfolder = steam_path + r"\steamapps\libraryfolders.vdf"
@@ -165,151 +163,10 @@ except:
     libraryfolder = None
     messagebox.showerror("error","Can't find steam libraries")
 
-#update the scales after switching setup type
-def chooseweatherType(*args):
-    idxs = lbox.curselection()
-    if len(idxs)==1:
-        idx = int(idxs[0])
-        country = countrynames[idx]
-        SelectTrack(country)
 
-
-def setAutoUseButton(*args):
-    with open('config.json') as json_file: 
-        config = json.load(json_file)     
-    json_file.close()
-
-    config['autoUseButton'] = autoUse.get()
-    
-    with open("config.json", "w") as json_file:
-        json.dump(config, json_file, indent=4)
-    json_file.close()
-
-#create widgets
-lbox = Listbox(c, 
-    listvariable=cnames, 
-    height=len(countrynames),
-    bg=bg,
-    fg=fg,
-    highlightcolor="black",
-    selectbackground="darkred",
-    selectforeground="white")
-lbox.selection_clear(1, last=None)
-raceBox = Combobox(c,
-    justify="center", 
-    values=race, 
-    state='disabled') #state= 'readonly' , 'disabled'
-carsBox = Combobox(c,
-    justify="center", 
-    values=cars, 
-    state='readonly') 
-weatherBox = Combobox(c,
-    justify="center", 
-    values=weather_, 
-    state='readonly') 
-status = ttk.Label(c, 
-    textvariable=statusmsg, 
-    anchor=W)
-check = ttk.Checkbutton(c, 
-    text='Auto Use', 
-    variable=autoUse, 
-    onvalue=True, 
-    offvalue=False,
-    command=setAutoUseButton)
-separatorV = ttk.Separator(c,
-    orient='vertical')
-
-# Grid all the widgets
-
-lbox.grid(
-    column=0,
-    row=0, 
-    rowspan=6, 
-    sticky=(N,S,E,W))
-raceBox.grid(
-    column=0, 
-    row=6, 
-    sticky=W)
-carsBox.grid(
-    column=0, 
-    row=7, 
-    sticky=W)
-weatherBox.grid(
-    column=0, 
-    row=8, 
-    sticky=W)
-check.grid(
-    column=0, 
-    row=11,
-    sticky=W,
-    padx=10)
-status.grid(
-    column=0, 
-    row=60, 
-    columnspan=6, 
-    sticky=(W,E))
-separatorV.grid(
-    column=1, 
-    row=0, 
-    rowspan=12, 
-    sticky=(N,S,E,W))
-c.grid_columnconfigure(0, weight=1)
-c.grid_rowconfigure(5, weight=1)
-
-
-# Colorize alternating lines of the listbox
-for i in range(0,len(countrynames),2):
-    lbox.itemconfigure(i, background='#576366',fg=fg)
-
-
-def MakeScale(from_ , to,  label, resolution=1):
-    scale = Scale(
-        c, 
-        from_=from_, 
-        to=to, 
-        orient=HORIZONTAL, 
-        label=label, 
-        length=scale_length,
-        relief=scale_relief, 
-        bg=bg,
-        fg=fg,
-        troughcolor=bg,
-        bd=3,
-        activebackground=bg,
-        highlightbackground=bg,
-        resolution=resolution)
-    return scale
-
-#create slider widgets (from_ , to,  label, resolution)
-front_wing_Scale = MakeScale(1, 11,"Front wing")
-rear_wing_Scale = MakeScale(1, 11, "Rear wing")
-on_throttle_Scale = MakeScale(50, 100, "On throttle")
-off_throttle_Scale = MakeScale(50, 100, "Off throttle")
-front_camber_Scale = MakeScale(-3.5, -2.5, "Front camber", 0.1) 
-rear_camber_Scale = MakeScale(-2, -1, "Rear camber", 0.1) 
-front_toe_Scale = MakeScale(0.05, 0.15, "Front toe", 0.01) 
-rear_toe_Scale = MakeScale(0.20, 0.50, "Rear toe", 0.03) #there is an offset
-front_suspension_Scale = MakeScale(1, 11, "Front suspension")
-rear_suspension_Scale = MakeScale(1, 11, "Rear suspension")
-front_antiroll_bar_Scale = MakeScale(1, 11, "Front antiroll bar")
-rear_antiroll_bar_Scale = MakeScale(1, 11, "Rear antiroll bar")
-
-front_suspension_height_Scale = MakeScale(1, 11, "Front suspension height")
-rear_suspension_height_Scale = MakeScale(1, 11, "Rear suspension height")
-brake_pressure_Scale = MakeScale(50, 100, "Brake pressure")
-brake_bias_Scale = MakeScale(70, 50, "Brake bias")
-front_right_tyre_pressure_Scale = MakeScale(21, 25, "Front right tyre pressure", 0.4) 
-front_left_tyre_pressure_Scale = MakeScale(21, 25, "Front left tyre pressure", 0.4) 
-rear_right_tyre_pressure_Scale = MakeScale(19.5, 23.5, "Rear right tyre pressure", .4) 
-rear_left_tyre_pressure_Scale = MakeScale(19.5, 23.5, "Rear left tyre pressure", 0.4) 
-ballast_Scale = MakeScale(1, 11 , "Ballast")
-fuel_load_Scale = MakeScale(5, 110, "Fuel load")
-ramp_differential_Scale = MakeScale(70,100, "Ramp differential")
-
-
-ri = 0 #set row integer
+#grid the sliders 
 def Scalegrid(scale):
-    global ri
+    global ri #row index
     row = ri
     column = 2
     maxrows = 11
@@ -318,7 +175,9 @@ def Scalegrid(scale):
         column = 4
     scale.grid(row=row, column=column, columnspan=2, sticky='NSEW')#needs own line
     ri += 1
-def forgetScales(): #remove the sliders 
+
+#remove ALL the sliders ,required if the race type changes(f1 , f2, etc)
+def forgetScales(): 
     front_wing_Scale.grid_forget()
     front_wing_Scale.grid_forget()
     rear_wing_Scale.grid_forget()
@@ -343,7 +202,9 @@ def forgetScales(): #remove the sliders
     ballast_Scale.grid_forget()
     fuel_load_Scale.grid_forget()
     ramp_differential_Scale.grid_forget()
-def gameModeScaleSettings(*args): #only grid the sliders that we can use for this mode
+
+#grid the sliders that we can use for this race type (f1, f2, etc)
+def gameModeScaleSettings(*args): 
     global ri
 
     race = raceBox.get()
@@ -433,22 +294,8 @@ def gameModeScaleSettings(*args): #only grid the sliders that we can use for thi
         Scalegrid(front_left_tyre_pressure_Scale)
         Scalegrid(rear_right_tyre_pressure_Scale)
         Scalegrid(rear_left_tyre_pressure_Scale)
-    
-def raceBoxSelected(*args):
-    global lboxCountry
-    forgetScales()
-    gameModeScaleSettings() #show/hide sliders
-    
-    carsBox['values'] = raceSettings[raceBox.get()] 
-    carsBox.current(0) #update carbox
-    SelectTrack(lboxCountry)
-def carsBoxSelected(*args):
-    global lboxCountry
-    SelectTrack(lboxCountry)
-def weatherBoxSelected(*args):
-    global lboxCountry
-    SelectTrack(lboxCountry)
 
+#set the current slider values
 def setScale(setup):
     front_wing_Scale.set(setup[41])
     rear_wing_Scale.set(setup[44])
@@ -473,8 +320,31 @@ def setScale(setup):
     ballast_Scale.set(setup[101])
     fuel_load_Scale.set(setup[104])
     ramp_differential_Scale.set(setup[107])
-def write():
+
+
+# combobox command functions  
+# comboxes need to remember previous selected track, as they become unselected.
+
+# when race type changes; show new sliders and update the cars in carsbox  
+def raceBoxSelected(*args):
+    global lboxCountry
+    forgetScales()
+    gameModeScaleSettings() #show/hide sliders
     
+    carsBox['values'] = raceSettings[raceBox.get()] 
+    carsBox.current(0) #update carbox
+    SelectTrack(lboxCountry)
+
+def carsBoxSelected(*args):
+    global lboxCountry
+    SelectTrack(lboxCountry)
+
+def weatherBoxSelected(*args):
+    global lboxCountry
+    SelectTrack(lboxCountry)
+
+#packed setup file with the current slider-values  -return setup
+def packSetup():
     setup = struct.pack(setupStructFormat, 
     b'F1CS', 0,1,0,32,0 ,7,                  #\x00\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00 \x00\x00\x00\x00\x00\x00\x00\x07
     b'versionsi32', 0, 9,                     #\x00\x00\x00\x00\t
@@ -512,66 +382,235 @@ def write():
     b'published_file_idui64', 31,174,162,128,0,0,0,0        # b'published_file_idui64\x1f\xae\xa2\x80\x00\x00\x00\x00'
     ) 
     
-    with open(root.filename, 'wb') as file:
+    return setup
+
+#write packedsetup to dir
+def writeSetup(filename):
+    setup = packSetup()
+    with open(filename, 'wb') as file:
         file.write(setup)
-    file.close()
+    file.close()    
 
-
-
-def Use(): #save the setup to a default file
-    saveFilename = root.filename #lets you save as track name after using
-    root.filename = WorkshopFile
-    write()
-    root.filename = saveFilename
+#write to WorkshopFile
+def Use(): 
+    writeSetup(WorkshopFile)
     statusmsg.set("Loaded  "+ root.filename)
 
+#write to both current file and workshop
 def UseSave():
-    write()
-    root.filename = WorkshopFile
-    write()
+    writeSetup(root.filename)
+    writeSetup(WorkshopFile)
     statusmsg.set("Saved Preset & loaded as "+ root.filename)
 
-def Save( ): #save the setup to a default file & overwrite the track save
-    write()
+#write to current file
+def Save( ): 
+    writeSetup(root.filename)
     statusmsg.set("Saved as: " + root.filename)
 
-def SaveAs(): #save the setup to a default file & save whereever you want
-    root.filename =  filedialog.asksaveasfilename(initialdir = F1SetupPath,title = "Select file",filetypes = (("bin files","*.bin"),("all files","*.*")))
-    write()
-    statusmsg.set("Saved: " + root.filename)
+#write to filedialogDir
+def SaveAs(): 
+    askedFilename =  filedialog.asksaveasfilename(initialdir = F1SetupPath,title = "Select file",filetypes = (("bin files","*.bin"),("all files","*.*")))
+    writeSetup(askedFilename)
+    statusmsg.set("Saved: " + askedFilename)
 
-def Open(): #open a setup fle
-    root.filename =  filedialog.askopenfilename(initialdir = F1SetupPath,title = "Select file",filetypes = (("bin files","*.bin"),("all files","*.*")))
-    #error if the file isnt a f1 2020 setup file
-    setupFile = open(root.filename, "rb")
-    setup = struct.unpack(setupStructFormat, setupFile.read())
-    name = setup[15]
-    statusmsg.set(str(name) + " Loaded (" + root.filename + ")")
-    setScale(setup)
+#open,unpack a setup.bin file and set sliders to files values          error if the file isnt a f1 2020 setup file
+def Open(): 
+    try:
+        root.filename =  filedialog.askopenfilename(initialdir = F1SetupPath,title = "Select file",filetypes = (("bin files","*.bin"),("all files","*.*")))
+        setupFile = open(root.filename, "rb")
+        setup = struct.unpack(setupStructFormat, setupFile.read())
+        statusmsg.set(" Opened (" + root.filename + ")")
+        setScale(setup)
+    except:
+        messagebox.showerror("error","can't open")
 
-def SelectTrack(country): #buttons will use this to load the track into the program
-    #error if the file isnt a f1 2020 setup file
+#select the file to open, unpack the file, update sliders, if autoUse is checked;  write to workshopfile
+def SelectTrack(country): 
     setupWeatherType = str(weatherType[weatherBox.get()])
     raceType = raceBox.get()
     carType = carsBox.get()
     setupType = weatherBox.get()
+    circuit = tracks[country]
     root.filename = SetupDir + raceType +'/'+ carType +'/'+ setupType +'/'+ country  +".bin"
-    
     setupFile = open(root.filename, "rb")
     setup = struct.unpack(setupStructFormat, setupFile.read())
-    circuit = tracks[country]
     setScale(setup)
-    
-    if autoUse.get() == True:
+    if autoUse.get() == True:   
         Use()
     statusmsg.set(" %s | %s | (%s) %s [%s]" % (raceType, carType, country, circuit, setupWeatherType))
 
-ttk.Button(c, text="Use", command=Use).grid(column=0, row=50, sticky=NSEW)
+#update the scales after switching setup type
+def chooseweatherType(*args):
+    idxs = lbox.curselection()
+    if len(idxs)==1:
+        idx = int(idxs[0])
+        country = countrynames[idx]
+        SelectTrack(country)
+
+#update config.json auto-use
+def setAutoUseButton(*args):
+    with open('config.json') as json_file: 
+        config = json.load(json_file)     
+    json_file.close()
+
+    config['autoUseButton'] = autoUse.get()
+    
+    with open("config.json", "w") as json_file:
+        json.dump(config, json_file, indent=4)
+    json_file.close()
+
+#create widgets
+lbox = Listbox(c, 
+    listvariable=cnames, 
+    height=len(countrynames),
+    bg=bg,
+    fg=fg,
+    highlightcolor="black",
+    selectbackground="darkred",
+    selectforeground="white")
+lbox.selection_clear(1, last=None)
+raceBox = Combobox(c,
+    justify="center", 
+    values=race, 
+    state='disabled') #state= 'readonly' , 'disabled'
+carsBox = Combobox(c,
+    justify="center", 
+    values=cars, 
+    state='readonly') 
+weatherBox = Combobox(c,
+    justify="center", 
+    values=weather_, 
+    state='readonly') 
+status = ttk.Label(c, 
+    textvariable=statusmsg, 
+    anchor=W)
+autoUseTrackBox = ttk.Checkbutton(c, 
+    text='Auto Use track', 
+    variable=autoUse, 
+    onvalue=True, 
+    offvalue=False,
+    command=setAutoUseButton)
+separatorV = ttk.Separator(c,
+    orient='vertical')
+
+#create Button widgets
+useButton = ttk.Button(
+    c, 
+    text="Use", 
+    command=Use
+    )
+useSaveButton = ttk.Button(
+    c,
+    text="Save & Use", 
+    command=UseSave
+    )
+saveButton = ttk.Button(
+    c, 
+    text="Save", 
+    command=Save
+    )
+saveAsButton = ttk.Button(
+    c, 
+    text="Save As", 
+    command=SaveAs
+    )
+openButton = ttk.Button(
+    c, 
+    text="Open", 
+    command=Open
+    )
+
+# Grid all the widgets
+
+lbox.grid(
+    column=0, row=0, 
+    rowspan=6, 
+    sticky=(N,S,E,W))
+raceBox.grid(
+    column=0, row=6, sticky=W)
+carsBox.grid(
+    column=0, row=7, sticky=W)
+weatherBox.grid(
+    column=0, row=8, sticky=W)
+autoUseTrackBox.grid(
+    column=0, row=11, sticky=W, padx=10)
+
+separatorV.grid(
+    column=1, row=0, 
+    rowspan=12, 
+    sticky=(N,S,E,W))
+
+#buttons
+useButton.grid(
+    column=0, row=50, sticky=NSEW)
 #column spacer
-ttk.Button(c, text="Save & Use", command=UseSave).grid(column=2, row=50, sticky=NSEW)
-ttk.Button(c, text="Save", command=Save).grid(column=3, row=50, sticky=NSEW)
-ttk.Button(c, text="Save As", command=SaveAs).grid(column=4, row=50, sticky=NSEW)
-ttk.Button(c, text="Open", command=Open).grid(column=5, row=50, sticky=NSEW)
+useSaveButton.grid(
+    column=2, row=50, sticky=NSEW)
+saveButton.grid(
+    column=3, row=50, sticky=NSEW)
+saveAsButton.grid(
+    column=4, row=50, sticky=NSEW)
+openButton.grid(
+    column=5, row=50, sticky=NSEW)
+
+#statusbar
+status.grid(
+    column=0, row=60, columnspan=6, sticky=(W,E))
+
+c.grid_columnconfigure(0, weight=1)
+c.grid_rowconfigure(5, weight=1)
+
+
+# Colorize alternating lines of the listbox
+for i in range(0,len(countrynames),2):
+    lbox.itemconfigure(i, background='#576366',fg=fg)
+
+#create sliders(scale) widgets - return  scale
+def MakeScale(from_ , to,  label, resolution=1):
+    scale = Scale(
+        c, 
+        from_=from_, 
+        to=to, 
+        orient=HORIZONTAL, 
+        label=label, 
+        length=scale_length,
+        relief=scale_relief, 
+        bg=bg,
+        fg=fg,
+        troughcolor=bg,
+        bd=3,
+        activebackground=bg,
+        highlightbackground=bg,
+        resolution=resolution)
+    return scale
+
+#slidername, create slider (from_ , to,  label, resolution)
+front_wing_Scale = MakeScale(1, 11,"Front wing")
+rear_wing_Scale = MakeScale(1, 11, "Rear wing")
+on_throttle_Scale = MakeScale(50, 100, "On throttle")
+off_throttle_Scale = MakeScale(50, 100, "Off throttle")
+front_camber_Scale = MakeScale(-3.5, -2.5, "Front camber", 0.1) 
+rear_camber_Scale = MakeScale(-2, -1, "Rear camber", 0.1) 
+front_toe_Scale = MakeScale(0.05, 0.15, "Front toe", 0.01) 
+rear_toe_Scale = MakeScale(0.20, 0.50, "Rear toe", 0.03) #there is an offset
+front_suspension_Scale = MakeScale(1, 11, "Front suspension")
+rear_suspension_Scale = MakeScale(1, 11, "Rear suspension")
+front_antiroll_bar_Scale = MakeScale(1, 11, "Front antiroll bar")
+rear_antiroll_bar_Scale = MakeScale(1, 11, "Rear antiroll bar")
+
+front_suspension_height_Scale = MakeScale(1, 11, "Front suspension height")
+rear_suspension_height_Scale = MakeScale(1, 11, "Rear suspension height")
+brake_pressure_Scale = MakeScale(50, 100, "Brake pressure")
+brake_bias_Scale = MakeScale(70, 50, "Brake bias")
+front_right_tyre_pressure_Scale = MakeScale(21, 25, "Front right tyre pressure", 0.4) 
+front_left_tyre_pressure_Scale = MakeScale(21, 25, "Front left tyre pressure", 0.4) 
+rear_right_tyre_pressure_Scale = MakeScale(19.5, 23.5, "Rear right tyre pressure", .4) 
+rear_left_tyre_pressure_Scale = MakeScale(19.5, 23.5, "Rear left tyre pressure", 0.4) 
+ballast_Scale = MakeScale(1, 11 , "Ballast")
+fuel_load_Scale = MakeScale(5, 110, "Fuel load")
+ramp_differential_Scale = MakeScale(70,100, "Ramp differential")
+
+
 
 # Called when the selection in the listbox changes; figure out
 # which country is currently selected, and then lookup its country
@@ -603,18 +642,15 @@ weather.set('Dry')
 statusmsg.set('')
 lbox.selection_set(0)
 
-#set the check button if autoUse is enabled
+#set the autoUseTrackBox button if autoUse is enabled
 with open('config.json') as json_file: 
     config = json.load(json_file)     
 json_file.close()
 
 if config['autoUseButton'] == True:
-    check.invoke()
+    autoUseTrackBox.invoke()
 
-
-
-
-#Assigning ComboBox Values
+#Assigning ComboBox default Values
 raceBox.current(0)
 carsBox.current(0) 
 weatherBox.current(0)
