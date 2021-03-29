cddd0679c3e96a856de884c255ce5cfaaaec747d
scruffe <78relyod@gmail.com>
2021-03-29 10:53:28 +0200

    trackname will update ingame

---
+++ trackId.py
@@ -0,0 +1,31 @@
+"""Track IDs
+ID Track
+
+Melbourne,
+Paul Ricard,
+Shanghai,
+Sakhir (Bahrain),
+Catalunya,
+Monaco,
+Montreal,
+Silverstone,
+Hockenheim,
+Hungaroring,
+Spa
+Monza
+Singapore
+Suzuka
+Abu Dhabi
+Texas
+Brazil
+Austria
+Sochi
+Mexico
+Baku (Azerbaijan),
+Sakhir Short,
+Silverstone Short,
+Texas Short,
+Suzuka Short,
+Hanoi,
+Zandvoort
+"""
